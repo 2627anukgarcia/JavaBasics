bd1e56c7e4a271924581f7c4511156c8f49b64d8
Anuk García <2627_anuk.garciaiticbcn.cat>
2026-09-22 10:40:45 +0200

    First commit

---
+++ 00_exercicisClase/NomEdat.java
@@ -0,0 +1,11 @@
+public class NomEdat{
+    public static void main(String[] args) {
+    String nom = "Anuk";
+    int edat = 17;
+    System.out.println ("em dic " + nom + " I  tinc " + edat + " anys");
+           
+
+
+
+        }
+}
