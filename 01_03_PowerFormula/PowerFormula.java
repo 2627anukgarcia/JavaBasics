10b03a6521100a3503381db6d6079cd1d237214a
Anuk García <2627_anuk.garciaiticbcn.cat>
2026-09-23 20:09:10 +0200

    Exercici 01_03

---
+++ 01_03_PowerFormula/PowerFormula.java
@@ -0,0 +1,15 @@
+public class PowerFormula {
+	public static void main(String[] args) {
+	
+	short force = 125;
+	
+	double distance = 37;
+	
+	int time = 12;
+	
+	double power = force*distance / time;
+	
+	System.out.println(power + " " + "W");
+	
+	}
+}
