a826d865da5e699200b3f9838d778cd4557c7953
Anuk García <2627_anuk.garciaiticbcn.cat>
2026-09-23 19:51:24 +0200

    First commit

---
+++ 01_02_CubicEquation/CubicEquation.java
@@ -0,0 +1,21 @@
+public class CubicEquation {
+
+         public static void main(String[] args) {
+
+         int x = 3;
+         long f = x*x*x+2*(x*x)+4*x+5 ;
+         System.out.println("f(3)"+" "+"="+" "+ f);
+
+        
+         x = 5;
+         f = x*x*x+2*(x*x)+4*x+5;
+         System.out.println( "f(5)"+" "+"="+" "+ f );
+
+
+
+         x = 10;
+         f = x*x*x+2*(x*x)+4*x+5;
+         System.out.println( "f(10)"+" "+"="+" "+ f);
+        
+         }
+}
