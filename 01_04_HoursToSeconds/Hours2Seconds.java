b3d52903dfb1e11b3780c7e138e6ec2a6136fa92
Anuk García <2627_anuk.garciaiticbcn.cat>
2026-09-25 08:40:25 +0200

    First commit

---
+++ 01_04_HoursToSeconds/Hours2Seconds.java
@@ -0,0 +1,24 @@
+public class Hours2Seconds{ 
+    
+	   public static void main (String[] args) {
+		   
+		   int hours = 12 ;
+		   int seconds = 3600;
+		   
+		   System.out.println("12 hours are"+" "+ hours*seconds+" " +"seconds");
+		   
+		   hours = 15;
+		   seconds = 3600;
+		   
+		   System.out.println("15 hours are"+" " + hours*seconds + " " +"seconds");
+		   
+		   hours = 23;
+		   seconds = 3600;
+		   
+		   System.out.println("23 hours are"+ " "+hours*seconds+" "+"seconds");
+		   
+		   
+		   
+		   
+	   }
+	}
